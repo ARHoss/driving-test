4bb9e99de9da3086057e51e6f1342127c8f48458
arhoss <rhossaincse@gmail.com>
2023-03-14 17:28:24 -0600

    updated-front-end

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,22 @@
-document.querySelector('#showAnswer').addEventListener('click', showAnswer);
-document.querySelector('.markComplete').addEventListener('click', markComplete);
+
+// Check if element with id "showAnswer" exists
+if (document.querySelector("#showAnswer")) {
+    // Attach event listener to element with id "showAnswer"
+    document.querySelector('#showAnswer').addEventListener('click', showAnswer);
+}
+
+// Check if element with class "markComplete" exists
+if (document.querySelector(".markComplete")) {
+    // Attach event listener to element with class "markComplete"
+    document.querySelector('.markComplete').addEventListener('click', markComplete);
+}
 
 
+
+
+// Event handler code goes here
 function showAnswer(){
-    const answer = document.querySelector('#answer');
+    const answer = document.querySelector('#questionAnswer');
     const answerIcon = document.querySelector('#answerIcon');
     answer.classList.toggle('is-hidden')
 
