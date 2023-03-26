35dea2fbdd2de057b5341f0e6480eaf7be34500e
arhoss <rhossaincse@gmail.com>
2023-03-26 11:03:44 -0600

    added icons for correct and wrong answer

--- public/js/main.js
+++ public/js/main.js
@@ -20,10 +20,10 @@ if (document.querySelector(".markComplete")) {
 // Event handler code goes here
 function showAnswer(e){
     e.preventDefault();
-    const answer = document.getElementsByClassName('questionAnswer');
+    const answer = document.querySelector('#questionAnswer');
     const answerIcon = document.querySelector('#answerIcon');
 
-    Array.from(answer).forEach(el => el.classList.toggle('is-hidden'))
+    answer.classList.toggle('is-hidden')
 
     if(answerIcon.classList.contains('fa-eye')){
         answerIcon.innerText = 'Show Answer';
@@ -49,7 +49,6 @@ function markComplete(){
 
 const answers = document.querySelectorAll('.answer');
 
-
 // Bind the event listener to each answer link
 answers.forEach(answer => {
   answer.addEventListener('click', findAnswer)
@@ -60,37 +59,48 @@ function findAnswer(e) {
     e.preventDefault();
 
     // Get the correct answer value from the hidden answer link
-    const answerLink = document.querySelector('.answerHidden');
+    const answerLink = document.querySelector('#realAnswer');
     const answerValue = answerLink.getAttribute('data-answer');
 
     // Get the user's selected answer value from the clicked answer link
     const answerSample = this.getAttribute('data-answer');
+    const answerDiv = this.parentElement;
 
     // Check if the user's selected answer is correct
     if (answerValue === answerSample) {
         this.classList.add('correct');
-        rightAnswer()
+        answerDiv.insertBefore(rightAnswer(), answerDiv.firstChild);
+        
     } else {
         this.classList.add('wrong');
-        wrongAnswer()
-
+        answerDiv.insertBefore(wrongAnswer(), answerDiv.firstChild);
+        
         // Highlight the correct answer
         answers.forEach(answer => {
             if (answer.getAttribute('data-answer') === answerValue) {
                 answer.classList.add('correct');
+                const answerDiv = answer.parentElement;
+                answerDiv.insertBefore(rightAnswer(), answerDiv.firstChild);
             }
         });
-
-        // Unbind the event listener from all answer links after the user has made a selection
-        answers.forEach(answer => {
-            answer.removeEventListener('click', findAnswer);
-        });
     }
+
+    // Unbind the event listener from all answer links after the user has made a selection
+    answers.forEach(answer => {
+        answer.removeEventListener('click', findAnswer);
+    });
 }
 
 function rightAnswer(){
 
     let score = localStorage.getItem('rightAnswer');
+
+    // Creating right answer span
+    const spanElement = document.createElement('span');
+    spanElement.setAttribute('class', 'icon has-text-success fa-lg mr-3');
+    const iElement = document.createElement('i');
+    iElement.setAttribute('class', 'fas fa-check-circle fa-2x');
+    spanElement.appendChild(iElement);
 
     if(score == null){
         localStorage.setItem('rightAnswer', 1)     
@@ -98,6 +108,8 @@ function rightAnswer(){
         score++
         localStorage.setItem('rightAnswer', score)
     }
+
+    return spanElement;
     
 }
 
@@ -105,11 +117,19 @@ function wrongAnswer(){
 
     let score = localStorage.getItem('wrongAnswer');
 
+    const spanElement = document.createElement('span');
+    spanElement.setAttribute('class', 'icon has-text-danger fa-lg mr-3');
+    const iElement = document.createElement('i');
+    iElement.setAttribute('class', 'fas fa-times-circle fa-2x');
+    spanElement.appendChild(iElement);
+
     if(score == null){
         localStorage.setItem('wrongAnswer', 1)     
     }else{
         score++
         localStorage.setItem('wrongAnswer', score)
     }
+
+    return spanElement;
     
 }
