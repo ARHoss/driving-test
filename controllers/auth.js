d0c6916f9aacb2ee52be271fbdd651a6fd895f20
arhoss <rhossaincse@gmail.com>
2023-09-16 22:40:21 -0600

    fixed login and signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,7 @@ exports.getLogin = (req, res) => {
   }
   res.render("login", {
     title: "Login",
+    currentPage:'login'
   });
 };
 
@@ -62,6 +63,7 @@ exports.getSignup = (req, res) => {
   }
   res.render("signup", {
     title: "Create Account",
+    currentPage:'signup'
   });
 };
 
